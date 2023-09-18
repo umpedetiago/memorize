e97c9e53d45cbaf3bda4f5b071c807931486ed90
umpedetiago <tiagopinheirobg@gmail.com>
2023-09-18 14:19:13 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by tiago pinheiro on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
